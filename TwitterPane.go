8984b821e8943dd3f75faa4b96364dc670558ad0
Lindsay Ward <lindsay.ward@jcu.edu.au>
2015-07-29 23:07:20 +1000

    added LED pane, double tap sends message but breaks LED connection (TCP issue?)

---
+++ TwitterPane.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"time"
+
+	"fmt"
+	"github.com/ninjasphere/gestic-tools/go-gestic-sdk"
+	"github.com/ninjasphere/sphere-go-led-controller/fonts/O4b03b"
+	"github.com/ninjasphere/sphere-go-led-controller/util"
+)
+
+var tapInterval = time.Millisecond * 500
+var introDuration = time.Millisecond * 1500
+
+// load a particular image - for a 'logo' in this case
+var imageLogo = util.LoadImage(util.ResolveImagePath("twitter-bird.png"))
+var imageAnimated = util.LoadImage(util.ResolveImagePath("twitter-animated.gif"))
+
+// LEDPane stores the data we want to access
+type LEDPane struct {
+	lastTap         time.Time
+	lastDoubleTap   time.Time
+	lastTapLocation gestic.Location
+
+	displayingIntro bool
+	introTimeout    *time.Timer
+	visible         bool
+
+	isImageMode  bool
+	imageIndex   int
+	currentImage util.Image
+	app          *TwitterApp
+}
+
+// NewDemoPane creates a DemoPane with the data and timers initialised
+// It doesn't need to do much more than create a struct if you want
+// the app is passed in so that the pane can access the data and methods in it
+func NewLEDPane(a *TwitterApp) *LEDPane {
+
+	pane := &LEDPane{
+		lastTap:      time.Now(),
+		isImageMode:  true,
+		imageIndex:   0,
+		app:          a,
+		currentImage: imageLogo,
+	}
+
+	pane.introTimeout = time.AfterFunc(0, func() {
+		log.Infof("introTimeout func firing...")
+		pane.displayingIntro = false
+	})
+
+	return pane
+}
+
+// Gesture is called by the system when the LED matrix receives any kind of gesture
+func (p *LEDPane) Gesture(gesture *gestic.GestureMessage) {
+	//	log.Infof("gesture received - %v, %v", gesture.Touch, gesture.Position)
+
+	// check the second last touch location since the most recent one before a tap is usually blank it seems
+	lastLocation := p.lastTapLocation
+	p.lastTapLocation = gesture.Touch
+
+	if gesture.Tap.Active() && time.Since(p.lastTap) > tapInterval {
+		p.lastTap = time.Now()
+
+		log.Infof("Tap! %v", lastLocation)
+
+		// check state of display to know what action to do
+		// TODO - states for actions (configured tweets)
+
+		// change between images - right or left
+		if lastLocation.East && !lastLocation.West {
+			p.currentImage = imageLogo
+		} else {
+			p.currentImage = imageAnimated
+		}
+	}
+
+	if gesture.DoubleTap.Active() && time.Since(p.lastDoubleTap) > tapInterval {
+		p.lastDoubleTap = time.Now()
+		log.Infof("Double Tap!")
+
+		p.currentImage = imageAnimated
+		//		result, err := p.app.twitterAPI.PostTweet("@lindsaymarkward is tweeting by tapping the #ninjasphere :)", nil)
+		result, err := p.app.twitterAPI.PostDMToScreenName("Bedtime :)", "@lindsaymarkward")
+		if err != nil {
+			log.Errorf("Error posting Tweet %v", err)
+		}
+		log.Infof("%v", result)
+
+		//		user, err := p.app.twitterAPI.GetSelf(nil)
+		//		if err != nil {
+		//			log.Infof("Error with Twitter API: %v", err)
+		//		}
+		//		log.Infof("Twitter API has self: %v", user.ScreenName)
+
+		// definitely shouldn't need this
+		p.app.SetupPane()
+	}
+}
+
+// KeepAwake is needed as it's part of the remote.pane interface
+func (p *LEDPane) KeepAwake() bool {
+	return true
+}
+
+// IsEnabled is needed as it's part of the remote.pane interface
+func (p *LEDPane) IsEnabled() bool {
+	return true
+}
+
+// Render is called by the system repeatedly when the pane is visible
+// It should return the RGBA image to be rendered on the LED matrix
+func (p *LEDPane) Render() (*image.RGBA, error) {
+
+	if !p.visible {
+		p.visible = true
+		p.displayingIntro = true
+		p.introTimeout.Reset(introDuration)
+	}
+
+	// simply return the logo image
+	if p.displayingIntro {
+		return imageLogo.GetNextFrame(), nil
+	}
+
+	// create an empty 16*16 RGBA image for the Draw function to draw into (to be returned)
+	img := image.NewRGBA(image.Rect(0, 0, 16, 16))
+
+	// display either images or some text
+	if p.isImageMode {
+		// set one of the images loaded at the start to be displayed
+		// Draw (built-in Go function) draws the frame from stateImg into the img 'image' starting at 4th parameter, "Over" the top
+		draw.Draw(img, img.Bounds(), p.currentImage.GetNextFrame(), image.Point{0, 0}, draw.Over)
+
+	} else {
+		// draw the index up the top
+		drawText(fmt.Sprintf("%2d", p.imageIndex), color.RGBA{10, 250, 250, 255}, 2, img)
+		// draw the text from app down the bottom
+		drawText(p.app.config.Username, color.RGBA{253, 151, 32, 255}, 9, img)
+		// add a border to the text (you can combine multiple images/text - just keep drawing into img
+	}
+
+	// return the image we've created to be rendered to the matrix
+	return img, nil
+}
+
+// drawText is a helper function to draw a string of text into an image
+func drawText(text string, col color.RGBA, top int, img *image.RGBA) {
+	width := O4b03b.Font.DrawString(img, 0, 8, text, color.Black)
+	start := int(16 - width - 1)
+
+	O4b03b.Font.DrawString(img, start, top, text, col)
+}
--- app.go
+++ app.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"net"
+	"os"
+
 	"github.com/ChimeraCoder/anaconda"
+	"github.com/lindsaymarkward/go-ninja/config"
 	"github.com/ninjasphere/go-ninja/api"
 	"github.com/ninjasphere/go-ninja/model"
 	"github.com/ninjasphere/go-ninja/support"
+	"github.com/ninjasphere/sphere-go-led-controller/remote"
 )
 
 var info = ninja.LoadModuleInfo("./package.json")
+var host = config.String("localhost", "led.host")
+var port = config.Int(3115, "led.remote.port")
 
 type TwitterApp struct {
 	support.AppSupport
+	led        *remote.Matrix
 	config     *TwitterAppModel
 	twitterAPI *anaconda.TwitterApi
 }
@@ -20,12 +29,13 @@ func (a *TwitterApp) Start(m *TwitterAppModel) error {
 	log.Infof("Starting Twitter app with config: %v", m)
 	a.config = m
 
-	a.SendEvent("config", m)
 	a.InitTwitterAPI()
 
 	a.Conn.MustExportService(&ConfigService{a}, "$app/"+a.Info.ID+"/configure", &model.ServiceAnnouncement{
 		Schema: "/protocol/configuration",
 	})
+
+	a.SetupPane()
 
 	return a.SendEvent("config", a.config)
 }
@@ -33,6 +43,32 @@ func (a *TwitterApp) Start(m *TwitterAppModel) error {
 // Stop
 func (a *TwitterApp) Stop() error {
 	return nil
+}
+
+func (a *TwitterApp) SetupPane() {
+	log.Infof("Making new pane...")
+	// The pane must implement the remote.pane interface
+	pane := NewLEDPane(a)
+
+	// Connect to the LED controller remote pane interface via TCP
+	log.Infof("Connecting to LED controller...")
+	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	// This creates a TCP connection, conn
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("DialTCP failed:", err.Error())
+		os.Exit(1)
+	}
+
+	log.Infof("Connected. Now making new matrix...")
+
+	// Export our pane over the TCP connection we just made
+	a.led = remote.NewMatrix(pane, conn)
 }
 
 func (a *TwitterApp) SaveAccount(data TwitterAppModel) error {
@@ -54,5 +90,11 @@ func (a *TwitterApp) InitTwitterAPI() error {
 	anaconda.SetConsumerKey(a.config.ConsumerKey)
 	anaconda.SetConsumerSecret(a.config.ConsumerSecret)
 	a.twitterAPI = anaconda.NewTwitterApi(a.config.AccessToken, a.config.AccessTokenSecret)
+	user, err := a.twitterAPI.GetSelf(nil)
+	if err != nil {
+		log.Infof("Error initialising Twitter API: %v", err)
+		return err
+	}
+	log.Infof("Initialised Twitter API with self: %v", user.ScreenName)
 	return nil
 }
