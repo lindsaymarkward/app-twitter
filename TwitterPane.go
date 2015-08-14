7946e9ee6c335d16298c11eae7f232bd82e2bf1e
Lindsay Ward <lindsay.ward@jcu.edu.au>
2015-08-14 20:46:16 +1000

    Before switching to new LED pane API

--- TwitterPane.go
+++ TwitterPane.go
@@ -12,15 +12,8 @@ import (
 )
 
 // TODO - states: error, each tweet, sending (with timer)
-// error: check API - can I draw animated red X over top of Twitter pic?
 
 var tapInterval = time.Millisecond * 500
-var introDuration = time.Millisecond * 1500
-
-// load state images
-var imageLogo = util.LoadImage(util.ResolveImagePath("twitter-bird.png"))
-var imageAnimated = util.LoadImage(util.ResolveImagePath("twitter-animated.gif"))
-var imageError = util.LoadImage(util.ResolveImagePath("error.gif"))
 
 // states
 const (
@@ -31,16 +24,25 @@ const (
 	Choosing
 )
 
+// load state images
+var images map[string]util.Image
+func init() {
+	images = make(map[string]util.Image)
+	images["logo"] = util.LoadImage(util.ResolveImagePath("twitter-bird.png"))
+	images["animated"] = util.LoadImage(util.ResolveImagePath("twitter-animated.gif"))
+	images["error"] = util.LoadImage(util.ResolveImagePath("canceling.gif")) // not...
+	images["at"] = util.LoadImage(util.ResolveImagePath("at.png"))
+}
+
 // LEDPane stores the data we want to access
 type LEDPane struct {
 	lastTap         time.Time
 	lastDoubleTap   time.Time
 	lastTapLocation gestic.Location
-	introTimeout    *time.Timer
-	visible         bool
 	currentImage    util.Image
 	app             *TwitterApp
 	state           int
+	updateTimer		*time.Timer
 }
 
 // NewLEDPane creates an LEDPane with the data and timers initialised
@@ -53,9 +55,22 @@ func NewLEDPane(a *TwitterApp) *LEDPane {
 		state:   Intro,
 	}
 
-	pane.introTimeout = time.AfterFunc(0, func() {
-		pane.state = Choosing
+	if !a.Initialised {
+		pane.state = ErrorAccount
+	}
+
+	pane.updateTimer = time.AfterFunc(0, func() {
+		if !a.Initialised {
+			pane.state = ErrorAccount
+		} else {
+			pane.state = Choosing
+		}
+		log.Infof("update timer running... state is %v", pane.state)
 	})
+
+//	pane.introTimeout = time.AfterFunc(0, func() {
+//		pane.state = Choosing
+//	})
 
 	return pane
 }
@@ -78,9 +93,9 @@ func (p *LEDPane) Gesture(gesture *gestic.GestureMessage) {
 
 		// change between images - right or left
 		if lastLocation.East && !lastLocation.West {
-			p.currentImage = imageLogo
+			p.currentImage = images["logo"]
 		} else {
-			p.currentImage = imageAnimated
+			p.currentImage = images["animated"]
 		}
 	}
 
@@ -90,14 +105,14 @@ func (p *LEDPane) Gesture(gesture *gestic.GestureMessage) {
 
 		// TODO - state with timer
 
-		p.currentImage = imageAnimated
+		p.currentImage = images["animated"]
 		// TODO - learn why I need "go" here or the LED connection gets lost
 		// ("WARNING matrix RemoteMatrix.go:70 Lost connection to led controller: EOF")
 		//		go p.app.PostDirectMessage("Nice one? Well, I hope so!", "@lindsaymarkward")
 	}
 }
 
-// KeepAwake sets whether the display fades (false) or not (true)
+// KeepAwake sets whether the display fades after 30 seconds (false) or stays on (true)
 func (p *LEDPane) KeepAwake() bool {
 	return false
 }
@@ -111,43 +126,35 @@ func (p *LEDPane) IsEnabled() bool {
 // It should return the RGBA image to be rendered on the LED matrix
 func (p *LEDPane) Render() (*image.RGBA, error) {
 
-	if !p.visible {
-		p.visible = true
-		p.state = Intro
-		p.introTimeout.Reset(introDuration)
-	}
+	log.Infof("State: %v", p.state)
 
-	if p.state == Intro {
-		p.currentImage = imageLogo
-		// simply return the logo image
-		return imageLogo.GetNextFrame(), nil
-	}
+//	p.UpdateStatus() // ??
+
 	// create an empty 16*16 RGBA image for the Draw function to draw into (to be returned)
 	img := image.NewRGBA(image.Rect(0, 0, 16, 16))
 
-	// TODO - where's the best place for this? Update function called less frequently?
-	if !p.app.Initialised {
-		p.state = ErrorAccount
-	} else {
-		p.state = Choosing
-	}
-
 	switch p.state {
-
+	case Intro:
+		return images["logo"].GetNextFrame(), nil
 	case Tweeting:
 	// animated
 	case Choosing:
 		// different tweet images/text
-		p.currentImage = imageLogo
+		if len(p.app.config.Tweets) == 0 {
+			// no tweets are saved
+
+		}
+		p.currentImage = images["logo"]
 	case ErrorAccount:
 		// @ with cross through it
-		p.currentImage = imageError
+		draw.Draw(img, img.Bounds(), images["at"].GetNextFrame(), image.Point{0, 0}, draw.Over)
+		draw.Draw(img, img.Bounds(), images["error"].GetNextFrame(), image.Point{0, 0}, draw.Over)
 	case ErrorTweet:
 		// (animated?) bird with cross
 	}
 
 	// Draw (built-in Go function) draws the frame from stateImg into the img 'image' starting at 4th parameter, "Over" the top
-	draw.Draw(img, img.Bounds(), p.currentImage.GetNextFrame(), image.Point{0, 0}, draw.Over)
+//	draw.Draw(img, img.Bounds(), p.currentImage.GetNextFrame(), image.Point{0, 0}, draw.Over)
 
 	//	// draw the index up the top
 	//	drawText(fmt.Sprintf("%2d", p.imageIndex), color.RGBA{10, 250, 250, 255}, 2, img)
@@ -164,4 +171,13 @@ func drawText(text string, col color.RGBA, top int, img *image.RGBA) {
 	start := int(16 - width - 1)
 
 	O4b03b.Font.DrawString(img, start, top, text, col)
+}
+
+func (p *LEDPane) UpdateStatus() {
+	// TODO - where's the best place for this? Update function called less frequently?
+	if !p.app.Initialised {
+		p.state = ErrorAccount
+	} else {
+		p.state = Choosing
+	}
 }
