d47c46a32015ec7cf8def26b39e982892defcce9
Lindsay Ward <lindsay.ward@jcu.edu.au>
2015-08-17 22:42:07 +1000

    All working. Using timer for taps so double tap registers before first tap is actioned

--- TwitterPane.go
+++ TwitterPane.go
@@ -14,8 +14,9 @@ import (
 )
 
 // TODO maybe - scrolling text for tweet name / messages?
+// TODO - start timer on tap, stop it on double-tap, do tap action if it fires
 
-var tapInterval = time.Millisecond * 900
+var tapInterval = time.Millisecond * 450
 var updateFrequency = time.Second * 2
 
 // states
@@ -42,58 +43,59 @@ func init() {
 
 // LEDPane stores the data we want to access
 type LEDPane struct {
-	lastTap            time.Time
-	lastDoubleTap      time.Time
-	lastTapLocation    gestic.Location
-	currentImage       util.Image
-	app                *TwitterApp
-	state              int
-	hasStoredTweets    bool
-	numberOfTweets     int
-	currentTweetNumber int
-	updateTimer        *time.Timer
+	lastTap              time.Time
+	lastDoubleTap        time.Time
+	lastTapLocation      gestic.Location
+	changeTweetDirection int
+	currentImage         util.Image
+	app                  *TwitterApp
+	state                int
+	hasStoredTweets      bool
+	numberOfTweets       int
+	currentTweetNumber   int
+	updateTimer          *time.Timer
+	tapTimer             *time.Timer
 }
 
 // NewLEDPane creates an LEDPane with the data and timers initialised
 // the app is passed in so that the pane can access the data and methods in it
 func NewLEDPane(a *TwitterApp) *LEDPane {
-	pane := &LEDPane{
+	p := &LEDPane{
 		lastTap:         time.Now(),
+		lastDoubleTap:   time.Now(),
 		app:             a,
 		hasStoredTweets: false,
+		numberOfTweets:  1, // to avoid divide by zero error the first time it's run
 	}
 
-	pane.updateTimer = time.AfterFunc(0, pane.UpdateStatus)
-
-	return pane
+	p.updateTimer = time.AfterFunc(0, p.UpdateStatus)
+	p.tapTimer = time.AfterFunc(0, p.TapAction)
+	return p
 }
 
 // Gesture is called by the system when the LED matrix receives any kind of gesture
 func (p *LEDPane) Gesture(gesture *gestic.GestureMessage) {
 	//	log.Infof("gesture received - %v, %v", gesture.Touch, gesture.Position)
+	//	log.Infof("Touch %v, Tap %v, Since: %v, Double %v, Since %v", gesture.Touch.Active(), gesture.Tap.Active(), time.Since(p.lastTap), gesture.DoubleTap.Active(), time.Since(p.lastDoubleTap))
 
 	// check the second last touch location because the most recent one before a tap is usually blank it seems
 	lastLocation := p.lastTapLocation
 	p.lastTapLocation = gesture.Touch
 
-	log.Infof("Tap %v, Since: %v, Double %v, Since %v", gesture.Tap.Active(), time.Since(p.lastTap), gesture.DoubleTap.Active(), time.Since(p.lastDoubleTap))
-
 	if gesture.Tap.Active() && time.Since(p.lastTap) > tapInterval {
 		p.lastTap = time.Now()
-
 		log.Infof("Tap! %v", lastLocation)
 
-		// check state of display to know what action to do
+		// do tap action only if we are in the right state
 		if p.state == Choosing && p.hasStoredTweets {
+			// start timer that will be stopped if double tap happens in time
+			// this avoids the problem of the first tap of a double being actioned as a tap
+			p.tapTimer.Reset(tapInterval)
 			// change between images - right or left
 			if lastLocation.West && !lastLocation.East {
-				p.currentTweetNumber -= 1
-				if p.currentTweetNumber < 0 {
-					p.currentTweetNumber = p.numberOfTweets - 1
-				}
+				p.changeTweetDirection = -1
 			} else {
-				p.currentTweetNumber += 1
-				p.currentTweetNumber %= p.numberOfTweets
+				p.changeTweetDirection = 1
 			}
 		}
 	}
@@ -102,6 +104,8 @@ func (p *LEDPane) Gesture(gesture *gestic.GestureMessage) {
 		p.lastDoubleTap = time.Now()
 		log.Infof("Double Tap!")
 		if p.state == Choosing {
+			// don't do tap action since we're double tapping
+			p.tapTimer.Stop()
 			// TODO - learn why I need "go" here or the LED connection gets lost
 			// ("WARNING matrix RemoteMatrix.go:70 Lost connection to led controller: EOF")
 			//		go p.app.PostDirectMessage("Nice one? I hope so!", "@lindsaymarkward")
@@ -125,7 +129,6 @@ func (p *LEDPane) IsEnabled() bool {
 // It should return the RGBA image to be rendered on the LED matrix
 func (p *LEDPane) Render() (*image.RGBA, error) {
 	//	log.Infof("State: %v", p.state)
-	//	p.UpdateStatus() // ??
 
 	// create an empty 16*16 RGBA image for the Draw function to draw into (to be returned)
 	img := image.NewRGBA(image.Rect(0, 0, 16, 16))
@@ -133,7 +136,7 @@ func (p *LEDPane) Render() (*image.RGBA, error) {
 	switch p.state {
 	case Tweeting:
 		draw.Draw(img, img.Bounds(), images["animated"].GetNextFrame(), image.Point{0, 0}, draw.Over)
-		O4b03b.Font.DrawString(img, 6, 3, fmt.Sprintf("%d", p.currentTweetNumber+1), color.RGBA{255, 255, 255, 255})
+		O4b03b.Font.DrawString(img, 6, 3, fmt.Sprintf("%d", p.currentTweetNumber+1), color.RGBA{20, 154, 233, 255})
 	case Choosing:
 		// different tweet numbers and DM or TWT text
 		draw.Draw(img, img.Bounds(), images["logo"].GetNextFrame(), image.Point{0, 0}, draw.Over)
@@ -199,6 +202,15 @@ func (p *LEDPane) UpdateStatus() {
 	p.updateTimer.Reset(updateFrequency)
 }
 
+// TapAction changes to the next/previous stored tweet (run on a timer when tapped)
+func (p *LEDPane) TapAction() {
+	p.currentTweetNumber += p.changeTweetDirection
+	p.currentTweetNumber %= p.numberOfTweets
+	if p.currentTweetNumber < 0 {
+		p.currentTweetNumber = p.numberOfTweets - 1
+	}
+}
+
 // tweetIt calls app's appropriate function to post tweet or direct message
 // should handle result and set state
 func (p *LEDPane) tweetIt() {
@@ -226,12 +238,11 @@ func (p *LEDPane) tweetIt() {
 	}
 	// handle error - success/fail display
 	if err != nil {
-//		log.Errorf(fmt.Sprintf("Tweetit error: %v", err))
+		//		log.Errorf(fmt.Sprintf("Tweetit error: %v", err))
 		p.state = TweetFailed
 	} else {
 		p.state = TweetSucceeded
 	}
-	// reset usual timer which will set state
+	// reset usual timer which will set state (so it displays success/fail for 2 seconds)
 	p.updateTimer.Reset(updateFrequency)
-
 }
