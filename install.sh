c3d2b4238778af1cb17588d785769428a86c4601
Lindsay Ward <lindsay.ward@jcu.edu.au>
2015-08-18 23:18:02 +1000

    Added installer bash script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+APPNAME="app-twitter"
+VERSION="0.1.0"
+LOCATION="apps"
+FILENAME=${APPNAME}.tar.gz
+
+echo "This script will download, install and run ${APPNAME}"
+#sudo with-rw bash
+cd /sphere/user-autostart/${LOCATION}
+eval wget https://github.com/lindsaymarkward/${APPNAME}/releases/download/${VERSION}/${FILENAME}
+mkdir ${APPNAME}
+tar -xf ${FILENAME} -C ${APPNAME}
+rm ${FILENAME}
+nservice -q ${APPNAME} start
+echo "Done... ${APPNAME} installed - hopefully :-)"
